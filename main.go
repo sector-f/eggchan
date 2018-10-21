764dd9fa0d9869f95e61ff8cced9942466a9e684
Adam <sector-f@users.noreply.github.com>
2018-10-20 22:00:15 -0400

    Cleaned up main.go

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
 import (
-	_ "fmt"
-	_ "github.com/gorilla/mux"
 	"os"
-	// "github.com/lib/pq"
-	_ "log"
 )
-
-// [x] List boards: `GET /boards`
-// [x] List threads on a board: `GET /boards/<name>`
-// [x] Display a thread: `GET /boards/<board>/<id>`
-// [ ] Post a thread: `POST /boards/<board>` (multipart/form-data using "comment" field)
-// [ ] Post a comment: `POST /boards/<board>/<id>` (multipart/form-data using "comment" field)
-// [x] List categories: `GET /categories`
-// [x] List boards in a category: `GET /categories/<category>`
 
 func main() {
 	app := App{}
