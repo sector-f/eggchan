c20e1f316ca136143099156c6ec3c19e5ca2f348
Adam <sector-f@users.noreply.github.com>
2018-10-22 16:03:21 -0400

    Switched to urfave/cli for arg parsing

--- main.go
+++ main.go
@@ -1,19 +1,54 @@
 package main
 
 import (
-	"flag"
+	"github.com/urfave/cli"
+	"log"
 	"os"
 )
 
 func main() {
-	addrPtr := flag.String("bind", "127.0.0.1:8000", "Address/port to bind to (Default: 127.0.0.1:8000)")
-	flag.Parse()
+	app := cli.NewApp()
+	app.Name = "eggchan"
+	app.Version = "0.1.0"
 
-	server := Server{}
-	server.Initialize(
-		os.Getenv("EGGCHAN_DB_USERNAME"),
-		os.Getenv("EGGCHAN_DB_PASSWORD"),
-		os.Getenv("EGGCHAN_DB_NAME"),
-	)
-	server.Run(*addrPtr)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "bind, b",
+			Value: "127.0.0.1:8000",
+			Usage: "Address/port to bind to",
+		},
+		cli.StringFlag{
+			Name:   "database, d",
+			Usage:  "Database name",
+			EnvVar: "EGGCHAN_DB_NAME",
+		},
+		cli.StringFlag{
+			Name:   "username, u",
+			Usage:  "Database username",
+			EnvVar: "EGGCHAN_DB_USERNAME",
+		},
+		cli.StringFlag{
+			Name:   "password, p",
+			Usage:  "Database password",
+			EnvVar: "EGGCHAN_DB_PASSWORD",
+		},
+	}
+
+	app.Action = func(ctx *cli.Context) error {
+		server := Server{}
+		server.Initialize(
+			ctx.String("username"),
+			ctx.String("password"),
+			ctx.String("database"),
+		)
+		server.Run(ctx.String("bind"))
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
