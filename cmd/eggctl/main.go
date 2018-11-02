fb4f42320ff8cb8a45ecaab5a6f3e3c37885f8a7
Adam <sector-f@users.noreply.github.com>
2018-11-02 16:40:27 -0400

    Added add-board subcommand

--- cmd/eggctl/main.go
+++ cmd/eggctl/main.go
@@ -14,6 +14,7 @@ func main() {
 		addUserCommand(),
 		listUsersCommand(),
 		deleteUserCommand(),
+		addBoardCommand(),
 		listBoardsCommand(),
 		listPermissionsCommand(),
 		grantPermissionsCommand(),
